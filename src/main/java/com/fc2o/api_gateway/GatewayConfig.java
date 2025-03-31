af45bf22f2cdb649358c24a2e281b3e7c8ed7edf
fcordonezo <fabiancamilo960720@gmail.com>
2025-03-31 12:30:46 -0500

    feat: Se agregan patrones de resiliencia a las rutas

--- src/main/java/com/fc2o/api_gateway/GatewayConfig.java
+++ src/main/java/com/fc2o/api_gateway/GatewayConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
+
 @Configuration
 public class GatewayConfig {
   @Bean
@@ -13,11 +15,41 @@ public class GatewayConfig {
     return builder.routes()
       .route(
         "orchestrator-service",
-        r -> r.path("/api/v1/orchestrator/**").uri("lb://VideogamesTournamentManagement")
+        r -> r.path("/api/v1/orchestrator/**")
+          .and()
+          .method(HttpMethod.PATCH, HttpMethod.DELETE, HttpMethod.POST)
+          .filters(f ->
+            f.retry(config ->
+                config
+                  .setRetries(3)
+                  .setBackoff(Duration.ofMillis(2000), Duration.ofMillis(6000), 2, true)
+              )
+              .circuitBreaker(config ->
+                config
+                  .setName("orchestratorCircuitBreaker")
+                  .setFallbackUri("forward:/fallback/orchestrator")
+              )
+          )
+          .uri("lb://VideogamesTournamentManagement")
       )
       .route(
         "airtable-service",
-        r -> r.path("/v0/**").and().method(HttpMethod.GET).uri("https://api.airtable.com")
+        r -> r.path("/v0/**")
+          .and()
+          .method(HttpMethod.GET)
+          .filters(f ->
+            f.retry(config ->
+                config
+                  .setRetries(3)
+                  .setBackoff(Duration.ofMillis(2000), Duration.ofMillis(6000), 3, true)
+              )
+              .circuitBreaker(config ->
+                config
+                  .setName("airtableCircuitBreaker")
+                  .setFallbackUri("forward:/fallback/airtable")
+              )
+          )
+          .uri("https://api.airtable.com")
       )
       .build();
   }
